887efc726ef2fdf422850be24143103b63fd7c95
FallMiner <52322048+PastMC@users.noreply.github.com>
2021-11-29 14:20:11 +0200

    Update Line.java

--- src/main/java/misterx/diamondgen/render/Line.java
+++ src/main/java/misterx/diamondgen/render/Line.java
@@ -7,7 +7,7 @@ import net.minecraft.client.render.VertexFormats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
-public class Line extends Renderer {
+public class Line extends Rendererv2 {
 
     public Vec3d start;
     public Vec3d end;
