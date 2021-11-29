1177bb1daf0dbc0b8a18b7817cad6e3885104029
FallMiner <52322048+PastMC@users.noreply.github.com>
2021-11-29 10:25:51 +0200

    Add files via upload

---
+++ src/main/java/misterx/diamondgen/DiamondGen.java
@@ -0,0 +1,36 @@
+package misterx.diamondgen;
+
+import misterx.diamondgen.render.RenderMain;
+import misterx.diamondgen.render.RenderQueue;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+public class DiamondGen implements ModInitializer {
+    public static int range = 100;
+    public static boolean active = true;
+    public static String ver = "1.17.0";
+    public static MinecraftClient client = MinecraftClient.getInstance();
+    public static boolean isOpaque() {
+        return opaque;
+    }
+
+    public static void setOpaque(boolean opaque) {
+        DiamondGen.opaque = opaque;
+    }
+
+    private static boolean opaque = false;
+
+    public static OreSim gen = new OreSim(0);
+    @Override
+    public void onInitialize() {
+        clear(0);
+        
+        RenderQueue.get().add("hand", RenderMain.get()::renderFinders);
+    }
+
+    public static void clear(long seed) {
+        gen = new OreSim(seed);
+        if (cient.getInstance().player == null)
+            return;
+        Util.reload();
+    }
+}
