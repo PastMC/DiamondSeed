238ddc784001a818678412b500e7f6b20ea62e66
FallMiner <52322048+PastMC@users.noreply.github.com>
2021-11-30 09:25:44 +0200

    Update DiamondGen.java

--- src/main/java/misterx/diamondgen/DiamondGen.java
+++ src/main/java/misterx/diamondgen/DiamondGen.java
@@ -29,7 +29,7 @@ public class DiamondGen implements ModInitializer {
         gen = new OreSim(seed);
         if (client.getInstance().player == null)
             return;
-        Utils.reload();
+        Util.reload();
     }
     public static void log(String log){
     	
